20ec6680ff6f4464990fa4eda2fb70366d972c98
class <ifaldmo@elpuig.xeill.net>
2025-10-10 18:22:31 +0200

    Initial commit

---
+++ src/Exercici1/examen.java
@@ -0,0 +1,34 @@
+package Exercici1;
+
+import multiplicallista.Multiplicacio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class examen {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
+        List<alumne> alumneList = new ArrayList<alumne>();
+        for (int i = 0; i < 25; i++) {
+            alumne a = new alumne("alumne-" + i);
+            alumneList.add(a);
+        }
+
+        List<Future<Integer>> notes;
+
+        notes = executor.invokeAll(alumneList);
+        executor.shutdown();
+
+        for(int i = 0; i < notes.size(); i++) {
+            Future<Integer> resultat = notes.get(i);
+            System.out.printf("%s ha tret un %d%n", alumneList.get(i).getNom(),resultat.get());
+        }
+
+
+
+    }
+}
---
+++ src/multiplicallista/ExamenAlumne.java
@@ -0,0 +1,5 @@
+package multiplicallista;
+
+public class ExamenAlumne {
+
+}
